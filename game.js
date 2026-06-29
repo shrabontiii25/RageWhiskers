8d6353cf07c3f37d585949b9a2789fc3dc9fba76
Shrabonti Majumder <smaj0010@student.monash.edu>
2026-06-29 13:21:23 +0800

    feat: update player movement and gravity

--- game.js
+++ game.js
@@ -15,5 +15,35 @@ document.addEventListener("keydown", (e) => {
     if (e.key === "ArrowRight") cat.speedX = 5;
     if (e.key === "ArrowLeft") cat.speedX = -5;
     if (e.key === "ArrowUp" && cat.onGround) cat.speedY = -12;
-};
+});
 
+document.addEventListener("keyup", (e) => {
+    if (e.key === "ArrowRight" || e.key === "ArrowLeft") cat.speedX = 0;
+});
+
+function gameloop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+// gravity 
+cat.speedY += 0.5; 
+cat.x += cat.speedX;
+cat.y += cat.speedY;
+
+
+// floor
+if (cat.y + cat.height > canvas.height) {
+    cat.y = canvas.height - cat.height;
+    cat.speedY = 0;
+    cat.onGround = true;
+} else {
+    cat.onGround = false;
+}
+
+// draw cat
+ctx.fillStyle = "orange";
+ctx.fillRect(cat.x, cat.y, cat.width, cat.height);
+
+requestAnimationFrame(gameloop);
+}
+
+gameloop();
